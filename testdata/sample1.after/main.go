7760c5c803f66782ab21430af4bc1abc45f47bcd
Assel Meher <asselmeher@gmail.com>
2020-04-03 14:55:17 +0100

    fix: correctly set expected result in samplepkg.after

--- testdata/sample1.after/main.go
+++ testdata/sample1.after/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-func B(ch chan int) {
+func B(ch chan<- int) {
 	ch <- 2
 }
 
-func BB(ch chan int) {
+func BB(ch <-chan int) {
 	for x := range ch {
 		_ = x
 	}
--- testdata/sample1.after/samplepkg1/samplepkg1.go
+++ testdata/sample1.after/samplepkg1/samplepkg1.go
@@ -1,9 +1,13 @@
 package samplepkg1
 
-func e(c chan int) {
+func e(c <-chan int) {
 	a := <-c
 }
 
-func a(c chan int) {
+func a(c <-chan int) {
 	a := <-c
+}
+
+func bb(c chan<- int) {
+	c <- 2
 }
